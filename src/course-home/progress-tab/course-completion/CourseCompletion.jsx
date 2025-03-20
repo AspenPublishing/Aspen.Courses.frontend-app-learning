eaa2bf95b22455c629578c0a63889b614a52240d
Amir Tadrisi <amirtds@gmail.com>
2025-03-19 21:00:09 -0500

    chore: add icons to progress page

--- src/course-home/progress-tab/course-completion/CourseCompletion.jsx
+++ src/course-home/progress-tab/course-completion/CourseCompletion.jsx
@@ -1,17 +1,110 @@
-import React from 'react';
-import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
+import React from "react";
+import { injectIntl, intlShape } from "@edx/frontend-platform/i18n";
 
-import CompletionDonutChart from './CompletionDonutChart';
-import messages from './messages';
+import CompletionDonutChart from "./CompletionDonutChart";
+import messages from "./messages";
 
 const CourseCompletion = ({ intl }) => (
   <section className="text-dark-700 mb-4 rounded raised-card p-4">
     <div className="row w-100 m-0">
       <div className="col-12 col-sm-6 col-md-7 p-0">
-        <h2>{intl.formatMessage(messages.courseCompletion)}</h2>
-        <p className="small">
-          {intl.formatMessage(messages.completionBody)}
-        </p>
+        <h2>
+          <svg
+            width="54"
+            height="54"
+            viewBox="0 0 54 54"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g filter="url(#filter0_d_8651_16804)">
+              <circle cx="27.1951" cy="24.2742" r="15.9236" fill="#FDBD1A" />
+              <circle
+                cx="27.1951"
+                cy="24.2742"
+                r="15.1609"
+                stroke="white"
+                stroke-width="1.5254"
+              />
+            </g>
+            <g clip-path="url(#clip0_8651_16804)">
+              <path
+                d="M26.8328 27.214C30.0282 27.214 32.6185 24.6237 32.6185 21.4283C32.6185 18.2329 30.0282 15.6426 26.8328 15.6426C23.6375 15.6426 21.0471 18.2329 21.0471 21.4283C21.0471 24.6237 23.6375 27.214 26.8328 27.214Z"
+                stroke="#000001"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M26.8329 23.9993C28.2531 23.9993 29.4043 22.848 29.4043 21.4279C29.4043 20.0077 28.2531 18.8564 26.8329 18.8564C25.4127 18.8564 24.2615 20.0077 24.2615 21.4279C24.2615 22.848 25.4127 23.9993 26.8329 23.9993Z"
+                stroke="#000001"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M25.7144 27.1109L24.5829 31.868C24.5605 31.9584 24.5191 32.043 24.4615 32.1162C24.4039 32.1893 24.3313 32.2494 24.2486 32.2923C24.1614 32.3302 24.0673 32.3497 23.9722 32.3497C23.8771 32.3497 23.783 32.3302 23.6958 32.2923L19.4272 30.428C19.3377 30.3857 19.2589 30.3236 19.1968 30.2465C19.1347 30.1694 19.0908 30.0792 19.0685 29.9827C19.0463 29.8862 19.0462 29.7859 19.0682 29.6893C19.0902 29.5927 19.1339 29.5024 19.1958 29.4252L22.5001 25.2852"
+                stroke="#000001"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M28.2859 27.0467L29.443 31.8682C29.466 31.9601 29.5091 32.0458 29.5691 32.1192C29.6291 32.1925 29.7046 32.2517 29.7902 32.2924C29.875 32.3308 29.9671 32.3506 30.0602 32.3506C30.1533 32.3506 30.2453 32.3308 30.3302 32.2924L34.573 30.4282C34.6639 30.3863 34.7438 30.3239 34.8063 30.2458C34.8688 30.1676 34.9122 30.076 34.933 29.9782C34.9574 29.8824 34.9585 29.7823 34.9361 29.6861C34.9137 29.5899 34.8686 29.5004 34.8045 29.4253L31.3587 25.041"
+                stroke="#000001"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </g>
+            <defs>
+              <filter
+                id="filter0_d_8651_16804"
+                x="0.66305"
+                y="0.0995815"
+                width="53.064"
+                height="53.0645"
+                filterUnits="userSpaceOnUse"
+                color-interpolation-filters="sRGB"
+              >
+                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <feColorMatrix
+                  in="SourceAlpha"
+                  type="matrix"
+                  values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                  result="hardAlpha"
+                />
+                <feOffset dy="2.35743" />
+                <feGaussianBlur stdDeviation="5.30422" />
+                <feComposite in2="hardAlpha" operator="out" />
+                <feColorMatrix
+                  type="matrix"
+                  values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0"
+                />
+                <feBlend
+                  mode="normal"
+                  in2="BackgroundImageFix"
+                  result="effect1_dropShadow_8651_16804"
+                />
+                <feBlend
+                  mode="normal"
+                  in="SourceGraphic"
+                  in2="effect1_dropShadow_8651_16804"
+                  result="shape"
+                />
+              </filter>
+              <clipPath id="clip0_8651_16804">
+                <rect
+                  width="18"
+                  height="18"
+                  fill="white"
+                  transform="translate(18 15)"
+                />
+              </clipPath>
+            </defs>
+          </svg>
+          {intl.formatMessage(messages.courseCompletion)}
+        </h2>
+        <p className="small">{intl.formatMessage(messages.completionBody)}</p>
       </div>
       <div className="col-12 col-sm-6 col-md-5 mt-sm-n3 p-0 text-center">
         <CompletionDonutChart />
--- src/course-home/progress-tab/grades/course-grade/CourseGrade.jsx
+++ src/course-home/progress-tab/grades/course-grade/CourseGrade.jsx
@@ -1,43 +1,111 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
+import React from "react";
+import { useSelector } from "react-redux";
+import { injectIntl, intlShape } from "@edx/frontend-platform/i18n";
 
-import { useModel } from '../../../../generic/model-store';
+import { useModel } from "../../../../generic/model-store";
 
-import CourseGradeFooter from './CourseGradeFooter';
-import CourseGradeHeader from './CourseGradeHeader';
-import GradeBar from './GradeBar';
-import CreditInformation from '../../credit-information/CreditInformation';
+import CourseGradeFooter from "./CourseGradeFooter";
+import CourseGradeHeader from "./CourseGradeHeader";
+import GradeBar from "./GradeBar";
+import CreditInformation from "../../credit-information/CreditInformation";
 
-import messages from '../messages';
+import messages from "../messages";
 
 const CourseGrade = ({ intl }) => {
-  const {
-    courseId,
-  } = useSelector(state => state.courseHome);
+  const { courseId } = useSelector((state) => state.courseHome);
 
   const {
     creditCourseRequirements,
     gradesFeatureIsFullyLocked,
     gradesFeatureIsPartiallyLocked,
-    gradingPolicy: {
-      gradeRange,
-    },
-  } = useModel('progress', courseId);
+    gradingPolicy: { gradeRange },
+  } = useModel("progress", courseId);
 
-  const passingGrade = Number((Math.min(...Object.values(gradeRange)) * 100).toFixed(0));
+  const passingGrade = Number(
+    (Math.min(...Object.values(gradeRange)) * 100).toFixed(0)
+  );
 
-  const applyLockedOverlay = gradesFeatureIsFullyLocked ? 'locked-overlay' : '';
+  const applyLockedOverlay = gradesFeatureIsFullyLocked ? "locked-overlay" : "";
 
   return (
     <section className="text-dark-700 my-4 rounded raised-card">
-      {(gradesFeatureIsFullyLocked || gradesFeatureIsPartiallyLocked) && <CourseGradeHeader />}
-      <div className={applyLockedOverlay} aria-hidden={gradesFeatureIsFullyLocked}>
+      {(gradesFeatureIsFullyLocked || gradesFeatureIsPartiallyLocked) && (
+        <CourseGradeHeader />
+      )}
+      <div
+        className={applyLockedOverlay}
+        aria-hidden={gradesFeatureIsFullyLocked}
+      >
         <div className="row w-100 m-0 p-4">
           <div className="col-12 col-sm-6 p-0 pr-sm-5.5">
-            <h2>{creditCourseRequirements
-              ? intl.formatMessage(messages.gradesAndCredit)
-              : intl.formatMessage(messages.grades)}
+            <h2>
+              <svg
+                width="54"
+                height="54"
+                viewBox="0 0 54 54"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g filter="url(#filter0_d_8651_16831)">
+                  <circle
+                    cx="27.1951"
+                    cy="24.2742"
+                    r="15.9236"
+                    fill="#FDBD1A"
+                  />
+                  <circle
+                    cx="27.1951"
+                    cy="24.2742"
+                    r="15.1609"
+                    stroke="white"
+                    stroke-width="1.5254"
+                  />
+                </g>
+                <path
+                  d="M33.1416 28.9299L32.7103 28.0277L33.1416 28.9299ZM33.5717 28.32H32.5717H33.5717ZM26.595 31.1609L26.1374 32.0501L26.595 31.1609ZM28.4236 31.1852L28.8549 32.0874L28.4236 31.1852ZM18.6662 21.7636L18.2317 20.863L18.6662 21.7636ZM18.6662 22.3721L18.2317 23.2728L18.6662 22.3721ZM27.948 26.4177L28.3825 27.3184L27.948 26.4177ZM27.0522 26.4177L27.4867 25.5171L27.0522 26.4177ZM36.334 21.7636L36.7685 20.863L36.334 21.7636ZM36.334 22.3721L35.8995 21.4715L36.334 22.3721ZM22.2655 28.933L22.723 28.0438L22.2655 28.933ZM27.948 17.718L27.5135 18.6187L27.948 17.718ZM27.0522 17.718L27.4867 18.6187L27.0522 17.718ZM37.4251 22.0561C37.4251 21.5038 36.9774 21.0561 36.4251 21.0561C35.8728 21.0561 35.4251 21.5038 35.4251 22.0561H37.4251ZM35.4251 25.8811C35.4251 26.4334 35.8728 26.8811 36.4251 26.8811C36.9774 26.8811 37.4251 26.4334 37.4251 25.8811H35.4251ZM27.5135 18.6187L35.8995 22.6643L36.7685 20.863L28.3825 16.8174L27.5135 18.6187ZM35.8995 21.4715L27.5135 25.5171L28.3825 27.3184L36.7685 23.2728L35.8995 21.4715ZM19.1007 22.6643L27.4867 18.6187L26.6176 16.8174L18.2317 20.863L19.1007 22.6643ZM27.4867 25.5171L22.2944 23.0122L21.4254 24.8135L26.6176 27.3184L27.4867 25.5171ZM22.2944 23.0122L19.1007 21.4715L18.2317 23.2728L21.4254 24.8135L22.2944 23.0122ZM20.8599 23.9128V28.3352H22.8599V23.9128H20.8599ZM21.8079 29.8222L26.1374 32.0501L27.0525 30.2717L22.723 28.0438L21.8079 29.8222ZM28.8549 32.0874L33.5728 29.8321L32.7103 28.0277L27.9923 30.283L28.8549 32.0874ZM34.5717 28.32L34.5717 23.9128L32.5717 23.9128L32.5717 28.32H34.5717ZM33.5728 29.8321C34.1104 29.5752 34.5717 29.0339 34.5717 28.32H32.5717C32.5717 28.1072 32.7059 28.0298 32.7103 28.0277L33.5728 29.8321ZM26.1374 32.0501C26.9726 32.4799 28.008 32.4922 28.8549 32.0874L27.9923 30.283C27.7058 30.4199 27.3282 30.4135 27.0525 30.2717L26.1374 32.0501ZM18.2317 20.863C17.7913 21.0754 17.4526 21.5159 17.4526 22.0679C17.4526 22.6199 17.7913 23.0604 18.2317 23.2728L19.1007 21.4715C19.2563 21.5465 19.4526 21.7514 19.4526 22.0679C19.4526 22.3843 19.2563 22.5892 19.1007 22.6643L18.2317 20.863ZM27.5135 25.5171C27.5175 25.5151 27.5181 25.5154 27.5148 25.5162C27.5115 25.5169 27.5063 25.5176 27.5001 25.5176C27.4938 25.5176 27.4887 25.5169 27.4854 25.5162C27.4821 25.5154 27.4827 25.5151 27.4867 25.5171L26.6176 27.3184C27.1682 27.584 27.832 27.584 28.3825 27.3184L27.5135 25.5171ZM35.8995 22.6643C35.7439 22.5892 35.5475 22.3843 35.5475 22.0679C35.5475 21.7514 35.7439 21.5465 35.8995 21.4715L36.7685 23.2728C37.2088 23.0604 37.5475 22.6199 37.5475 22.0679C37.5475 21.5159 37.2088 21.0754 36.7685 20.863L35.8995 22.6643ZM20.8599 28.3352C20.8599 29.0247 21.2921 29.5568 21.8079 29.8222L22.723 28.0438C22.7337 28.0493 22.8599 28.1304 22.8599 28.3352H20.8599ZM28.3825 16.8174C27.832 16.5518 27.1682 16.5518 26.6176 16.8174L27.4867 18.6187C27.4827 18.6206 27.4821 18.6203 27.4854 18.6196C27.4887 18.6189 27.4938 18.6182 27.5001 18.6182C27.5063 18.6182 27.5115 18.6189 27.5148 18.6196C27.5181 18.6203 27.5175 18.6206 27.5135 18.6187L28.3825 16.8174ZM35.4251 22.0561V25.8811H37.4251V22.0561H35.4251Z"
+                  fill="black"
+                />
+                <defs>
+                  <filter
+                    id="filter0_d_8651_16831"
+                    x="0.66305"
+                    y="0.0995815"
+                    width="53.064"
+                    height="53.0645"
+                    filterUnits="userSpaceOnUse"
+                    color-interpolation-filters="sRGB"
+                  >
+                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                    <feColorMatrix
+                      in="SourceAlpha"
+                      type="matrix"
+                      values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                      result="hardAlpha"
+                    />
+                    <feOffset dy="2.35743" />
+                    <feGaussianBlur stdDeviation="5.30422" />
+                    <feComposite in2="hardAlpha" operator="out" />
+                    <feColorMatrix
+                      type="matrix"
+                      values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0"
+                    />
+                    <feBlend
+                      mode="normal"
+                      in2="BackgroundImageFix"
+                      result="effect1_dropShadow_8651_16831"
+                    />
+                    <feBlend
+                      mode="normal"
+                      in="SourceGraphic"
+                      in2="effect1_dropShadow_8651_16831"
+                      result="shape"
+                    />
+                  </filter>
+                </defs>
+              </svg>
+              {creditCourseRequirements
+                ? intl.formatMessage(messages.gradesAndCredit)
+                : intl.formatMessage(messages.grades)}
             </h2>
             <p className="small">
               {intl.formatMessage(messages.courseGradeBody)}
